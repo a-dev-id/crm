cb565fae6fd2745ff14e203bfd4ff041c73c24a9
A-Dev <3xasov@gmail.com>
2022-09-12 17:48:46 +0800

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,5 @@ mix.js("resources/js/app.js", "public/js")
     .postCss("resources/css/app.css", "public/css", [
         //
     ])
-    .browserSync("127.0.0.1:8000");
+    .browserSync("127.0.0.1:8000")
+    .disableSuccessNotifications();
